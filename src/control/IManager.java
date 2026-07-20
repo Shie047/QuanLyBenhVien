f7289f653856ed9874b1036f2e28cfbe5cde5901
Minh Duy <tongminhduynhut@gmail.com>
2026-07-21 00:16:51 +0700

    Delete src/control/IManager.java

--- src/control/IManager.java
+++
@@ -1,9 +0,0 @@
-package control;
-import java.util.List;
-
-public interface IManager<T> {
-    String add(T entity);
-    String update(String code, T newEntity);
-    String delete(String code);
-    List<T> getAll();
-}
